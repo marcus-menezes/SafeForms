338102648d0157274412d1c09a2e67b57939e566
Marcus Menezes <marcusmenezes2009@gmail.com>
2024-06-06 12:06:22 -0300

    feat(form): create forn model

---
+++ frontend/src/models/form.ts
@@ -0,0 +1,11 @@
+export interface IForm {
+  name: string;
+  email: string;
+  birthdate: string;
+  description: string;
+  cpf: string;
+  country: string;
+  state: string;
+  city: string;
+  files: string;
+}
