ac09656e1da5a1f4ac0c562845d19bcf879748bb
Marcus Menezes <marcusmenezes2009@gmail.com>
2024-06-07 17:33:37 -0300

    feat(base.service): remove some functions

--- frontend/src/services/base.service.ts
+++ frontend/src/services/base.service.ts
@@ -1,21 +1,5 @@
 import axios from "axios";
-import { deleteCookie, getCookie } from "cookies-next";
-import { useRouter } from "next/navigation";
-
-export const HttpStatus = {
-  OK: 200,
-  CREATED: 201,
-  NO_CONTENT: 204,
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
-  FORBIDDEN: 403,
-  NOT_FOUND: 404,
-  CONFLICT: 409,
-  INTERNAL_SERVER_ERROR: 500,
-  BAD_GATEWAY: 502,
-  SERVICE_UNAVAILABLE: 503,
-  GATEWAY_TIMEOUT: 504,
-};
+import { getCookie } from "cookies-next";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -36,27 +20,5 @@ api.interceptors.request.use(
 
     return config;
   },
-  (error) => {
-    console.log({
-      error,
-    });
-    return Promise.reject(error);
-  }
-);
-
-api.interceptors.response.use(
-  async (config) => {
-    if (config.status === HttpStatus.UNAUTHORIZED) {
-      deleteCookie("token");
-      useRouter().push("/home");
-    }
-
-    return config;
-  },
-  (error) => {
-    console.log({
-      error,
-    });
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
